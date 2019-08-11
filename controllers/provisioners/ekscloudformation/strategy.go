166644d7003bd42b7a5dc0d9d0cef57a9bf0a07f
Eytan Avisror <eytan_avisror@intuit.com>
2019-08-11 16:50:48 -0700

    feature

--- controllers/provisioners/ekscloudformation/strategy.go
+++ controllers/provisioners/ekscloudformation/strategy.go
@@ -30,21 +30,79 @@ import (
 	"k8s.io/apimachinery/pkg/runtime/schema"
 )
 
+const (
+	OwnershipAnnotationKey   = "instancemgr.orkaproj.io/owned"
+	DefaultConcurrencyPolicy = "forbid"
+)
+
+func (ctx *EksCfInstanceGroupContext) discoverCreatedResources(s schema.GroupVersionResource, namespace, currentResourceName string) error {
+	var (
+		instanceGroup         = ctx.GetInstanceGroup()
+		spec                  = &instanceGroup.Spec
+		strategyConfiguration = spec.AwsUpgradeStrategy.GetCRDType()
+		state                 = ctx.GetDiscoveredState()
+		statusJSONPath        = strategyConfiguration.GetStatusJSONPath()
+		completedStatus       = strategyConfiguration.GetStatusSuccessString()
+		errorStatus           = strategyConfiguration.GetStatusFailureString()
+	)
+
+	resources, err := ctx.KubernetesClient.KubeDynamic.Resource(s).Namespace(namespace).List(metav1.ListOptions{})
+	if err != nil {
+		return err
+	}
+
+	for _, resource := range resources.Items {
+		if hasAnnotation(&resource, OwnershipAnnotationKey, "true") {
+			state.AddOwnedResources(resource)
+		}
+	}
+
+	for _, resource := range state.GetOwnedResources() {
+		if resource.GetName() == currentResourceName {
+			break
+		}
+		val, err := getUnstructuredPath(resource, statusJSONPath)
+		if err != nil {
+			return err
+		}
+		if val != completedStatus && val != errorStatus {
+			// if resource is not completed and not failed, it must be still active
+			log.Infof("found active owned resource: %v", resource.GetName())
+			state.AddActiveOwnedResources(resource)
+		}
+	}
+	return nil
+}
+
 func (ctx *EksCfInstanceGroupContext) processCRDStrategy() error {
-	var customResourceKind string
-	var customResourceName string
-	var customResourceNamespace string
-	var instanceGroup = ctx.GetInstanceGroup()
-	var spec = &instanceGroup.Spec
-	var status = &instanceGroup.Status
-	var strategyConfiguration = spec.AwsUpgradeStrategy.GetCRDType()
-	var state = ctx.GetDiscoveredState()
-	var statusJSONPath = strategyConfiguration.GetStatusJSONPath()
-	var completedStatus = strategyConfiguration.GetStatusSuccessString()
-	var errorStatus = strategyConfiguration.GetStatusFailureString()
-	var customResourceSpec = strategyConfiguration.GetSpec()
-	var customResourceDefinitionName = strategyConfiguration.GetCRDName()
-	var selfGroup = state.GetSelfGroup()
+
+	var (
+		customResourceKind           string
+		customResourceName           string
+		customResourceNamespace      string
+		instanceGroup                = ctx.GetInstanceGroup()
+		spec                         = &instanceGroup.Spec
+		status                       = &instanceGroup.Status
+		strategyConfiguration        = spec.AwsUpgradeStrategy.GetCRDType()
+		state                        = ctx.GetDiscoveredState()
+		statusJSONPath               = strategyConfiguration.GetStatusJSONPath()
+		completedStatus              = strategyConfiguration.GetStatusSuccessString()
+		errorStatus                  = strategyConfiguration.GetStatusFailureString()
+		customResourceSpec           = strategyConfiguration.GetSpec()
+		customResourceDefinitionName = strategyConfiguration.GetCRDName()
+		concurrencyPolicy            = strategyConfiguration.GetConcurrencyPolicy()
+		selfGroup                    = state.GetSelfGroup()
+	)
+
+	if concurrencyPolicy == "" {
+		concurrencyPolicy = DefaultConcurrencyPolicy
+		strategyConfiguration.SetConcurrencyPolicy(concurrencyPolicy)
+	}
+
+	if !common.ContainsString([]string{"forbid", "allow"}, strings.ToLower(concurrencyPolicy)) {
+		err := fmt.Errorf("invalid concurrencyPolicy provided: %v", concurrencyPolicy)
+		return err
+	}
 
 	if customResourceSpec == "" {
 		err := fmt.Errorf("custom resource spec not provided")
@@ -68,8 +126,14 @@ func (ctx *EksCfInstanceGroupContext) processCRDStrategy() error {
 		return err
 	}
 
+	addAnnotation(customResource, OwnershipAnnotationKey, "true")
+
 	API := customResource.GetAPIVersion()
 	s := strings.Split(API, "/")
+	if len(s) != 2 {
+		err = fmt.Errorf("malformed apiVersion provided: %v, expecting 'some.api.com/v1'", API)
+		return err
+	}
 	customResourceDefinitionGroup, customResourceDefinitionVersion := s[0], s[1]
 
 	if strings.HasSuffix(customResourceDefinitionName, customResourceDefinitionGroup) {
@@ -113,6 +177,12 @@ func (ctx *EksCfInstanceGroupContext) processCRDStrategy() error {
 		Resource: customResourceKind,
 	}
 
+	err = ctx.discoverCreatedResources(customResourceDefinitionSchema, customResourceNamespace, customResourceName)
+	if err != nil {
+		log.Errorf("failed to discover active custom resources: %v", err)
+		return err
+	}
+
 	customResourceDefinitionFullName := strings.Join([]string{customResourceKind, customResourceDefinitionGroup}, ".")
 
 	if !common.CRDExists(ctx.KubernetesClient.KubeDynamic, customResourceDefinitionFullName) {
@@ -120,8 +190,15 @@ func (ctx *EksCfInstanceGroupContext) processCRDStrategy() error {
 		return err
 	}
 
-	log.Infoln("submitting custom resource")
+	if len(state.GetActiveOwnedResources()) != 0 {
+		if strings.ToLower(concurrencyPolicy) == "forbid" {
+			log.Infoln("custom resource/s still active, will wait for finite-state per concurrencyPolicy = Forbid")
+			instanceGroup.SetState(v1alpha1.ReconcileModifying)
+			return nil
+		}
+	}
 
+	log.Infoln("submitting custom resource")
 	customResource, err = ctx.submitCustomResource(customResourceDefinitionSchema, customResource)
 	if err != nil {
 		log.Errorln("failed to submit upgrade CR")
@@ -205,24 +282,23 @@ func (ctx *EksCfInstanceGroupContext) checkCustomResourceField(JSONPath string, 
 		return false, err
 	}
 
-	splitFunction := func(c rune) bool {
-		return c == '.'
+	value, err := getUnstructuredPath(customResource, JSONPath)
+	if err != nil {
+		return false, err
 	}
-	statusPath := strings.FieldsFunc(JSONPath, splitFunction)
 
-	value, ok, err := unstructured.NestedString(customResource.UnstructuredContent(), statusPath...)
-	if ok {
-		switch strings.ToLower(value) {
-		case strings.ToLower(successString):
-			return true, nil
-		case strings.ToLower(errorString):
-			err := fmt.Errorf("custom resource failed to converge, %v status is %v", JSONPath, value)
-			return false, err
-		default:
-			return false, nil
-		}
-	} else {
+	if value == "" {
 		log.Warnf("could not find %v in custom resource", JSONPath)
+		return false, nil
+	}
+
+	switch strings.ToLower(value) {
+	case strings.ToLower(successString):
+		return true, nil
+	case strings.ToLower(errorString):
+		err := fmt.Errorf("custom resource failed to converge, %v status is %v", JSONPath, value)
+		return false, err
+	default:
 		return false, nil
 	}
 }
