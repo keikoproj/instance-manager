03464d8bc3bb4f7c8ece7ebc9cdee7ce71b3c9d4
Eytan Avisror <eytan_avisror@intuit.com>
2019-10-30 14:00:56 -0700

    syntax fix

--- controllers/provisioners/ekscloudformation/ekscloudformation_test.go
+++ controllers/provisioners/ekscloudformation/ekscloudformation_test.go
@@ -1274,9 +1274,9 @@ func TestGetNodeAutoScalingGroupMetrics(t *testing.T) {
 
 func TestParseCustomResourceYamlEmptyString(t *testing.T) {
 	empty, err := common.ParseCustomResourceYaml("")
-	if (len(empty.Object) > 0)  {
-		t.Fatalf("Expected ParseCustomResourceYaml to return Unstructured whose Unstructured.object is of length: 0 but" +
-			" instead object is of length %d", len(empty.Object));
+	if len(empty.Object) > 0 {
+		t.Fatalf("Expected ParseCustomResourceYaml to return Unstructured whose Unstructured.object is of length: 0 but"+
+			" instead object is of length %d", len(empty.Object))
 	}
 	if err != nil {
 		t.Fatal("Empty YAML string produces error")
