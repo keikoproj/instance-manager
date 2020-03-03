c13b4ec14e704e3dd47e816802c253f3356657c5
twhitcomb <tom_whitcomb@intuit.com>
2020-03-03 14:22:05 -0800

    state machine for creating/deleting fargate profiles

--- controllers/provisioners/eksfargate/eksfargate.go
+++ controllers/provisioners/eksfargate/eksfargate.go
@@ -23,6 +23,25 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
+func hash(instancegroup_state string, profile_status string) string {
+	return instancegroup_state + ":" + profile_status
+}
+
+type transition func(ig *v1alpha1.InstanceGroup, worker awsprovider.AwsFargateWorker) error
+type stateMachine map[string]transition
+
+var sm = stateMachine{
+	// IG state    Profile State
+	hash("", "NONE"):     t1,
+	hash("", "ACTIVE"):   e1,
+	hash("", "CREATING"): e1,
+	hash("", "DELETING"): e1,
+	hash(string(v1alpha1.ReconcileInitCreate), "ACTIVE"): t2,
+	hash(string(v1alpha1.ReconcileDeleting), "NONE"):     t3,
+	hash(string(v1alpha1.ReconcileInit), "NONE"):         t4,
+	hash(string(v1alpha1.ReconcileReady), "ACTIVE"):      t5,
+}
+
 func New(instanceGroup *v1alpha1.InstanceGroup) (*EksFargateInstanceGroupContext, error) {
 	ctx := EksFargateInstanceGroupContext{
 		InstanceGroup: instanceGroup,
@@ -42,6 +61,64 @@ func createFargateSelectors(selectors []*v1alpha1.EKSFargateSelectors) []*eks.Fa
 	return eksSelectors
 }
 
+func e1(ig *v1alpha1.InstanceGroup, worker awsprovider.AwsFargateWorker) error {
+	log.Info("Running transistioner e1\n")
+	ig.SetState(v1alpha1.ReconcileErr)
+	return nil
+}
+func t1(ig *v1alpha1.InstanceGroup, worker awsprovider.AwsFargateWorker) error {
+	log.Info("Running transistioner t1\n")
+	var err error = nil
+	// cluster and profile do not exist
+	//Not a delete
+	if ig.ObjectMeta.DeletionTimestamp.IsZero() {
+		ig.SetState(v1alpha1.ReconcileInit)
+	} else {
+		ig.SetState(v1alpha1.ReconcileErr)
+	}
+	return err
+}
+func t2(ig *v1alpha1.InstanceGroup, worker awsprovider.AwsFargateWorker) error {
+	log.Info("Running transistioner t2\n")
+	ig.SetState(v1alpha1.ReconcileReady)
+	return nil
+}
+func t3(ig *v1alpha1.InstanceGroup, worker awsprovider.AwsFargateWorker) error {
+	log.Info("Running transistioner t3\n")
+	ig.SetState(v1alpha1.ReconcileDeleted)
+	return nil
+}
+func t4(ig *v1alpha1.InstanceGroup, worker awsprovider.AwsFargateWorker) error {
+	log.Info("Running transistioner t4\n")
+	var err error = nil
+	// Create request
+	if ig.ObjectMeta.DeletionTimestamp.IsZero() {
+		// profile does not exist
+		log.Infof("Fargate creating cluster: %s and profile: %s \n", *worker.ClusterName, *worker.ProfileName)
+		err = worker.Create()
+		if err != nil {
+			ig.SetState(v1alpha1.ReconcileErr)
+		} else {
+			ig.SetState(v1alpha1.ReconcileInitCreate)
+		}
+	}
+	return err
+}
+func t5(ig *v1alpha1.InstanceGroup, worker awsprovider.AwsFargateWorker) error {
+	log.Info("Running transistioner t5\n")
+	var err error = nil
+	// Delete request
+	if !ig.ObjectMeta.DeletionTimestamp.IsZero() {
+		err = worker.Delete()
+		if err != nil {
+			ig.SetState(v1alpha1.ReconcileErr)
+		} else {
+			ig.SetState(v1alpha1.ReconcileDeleting)
+		}
+	}
+	return err
+}
+
 func (ctx *EksFargateInstanceGroupContext) HandleRequest() error {
 	log.SetFormatter(&log.TextFormatter{
 		FullTimestamp: true,
@@ -55,75 +132,24 @@ func (ctx *EksFargateInstanceGroupContext) HandleRequest() error {
 	}
 
 	ig := ctx.GetInstanceGroup()
-	log.Infof("Fargate state: %s", ig.GetState())
-	log.Infof("Fargate DeletionTimestamp: %v \n", ig.ObjectMeta.DeletionTimestamp)
 
 	fargateProfile, fargateErr := worker.Describe()
+
+	var profileStatus = "NONE"
 	if fargateErr != nil {
 		log.Infof("Fargate cluster: %s and profile: %s not found\n", *worker.ClusterName, *worker.ProfileName)
 	} else {
-		log.Infof("Fargate cluster: %s and profile: %s has status %s\n", *worker.ClusterName, *worker.ProfileName, *fargateProfile.Status)
-	}
-	// first call
-	if ig.GetState() == "" {
-		var err error = nil
-		// cluster and profile do not exist
-		if fargateErr != nil {
-			//Not a delete
-			if ig.ObjectMeta.DeletionTimestamp.IsZero() {
-				ig.SetState(v1alpha1.ReconcileInit)
-			} else {
-				ig.SetState(v1alpha1.ReconcileErr)
-			}
-		} else {
-			ig.SetState(v1alpha1.ReconcileErr)
-		}
-		return err
+		log.Infof("Fargate cluster: %s and profile: %s status %s\n", *worker.ClusterName, *worker.ProfileName, *fargateProfile.Status)
+		profileStatus = *fargateProfile.Status
 	}
 
-	if ig.GetState() == v1alpha1.ReconcileInitCreate {
-		var err error = nil
-		if *fargateProfile.Status == "ACTIVE" {
-			ig.SetState(v1alpha1.ReconcileReady)
-		}
-		return err
-	}
-	if ig.GetState() == v1alpha1.ReconcileDeleting {
-		var err error = nil
-		if fargateErr != nil {
-			ig.SetState(v1alpha1.ReconcileDeleted)
-		}
-		return err
-	}
-	if ig.GetState() == v1alpha1.ReconcileInit {
-		var err error = nil
-		// Create request
-		if ig.ObjectMeta.DeletionTimestamp.IsZero() {
-			// profile does not exist
-			log.Infof("Fargate creating cluster: %s and profile: %s \n", *worker.ClusterName, *worker.ProfileName)
-			err = worker.Create()
-			if err != nil {
-				ig.SetState(v1alpha1.ReconcileErr)
-			} else {
-				ig.SetState(v1alpha1.ReconcileInitCreate)
-			}
-		}
-		return err
-	}
+	log.Infof("Fargate DeletionTimestamp: %v \n", ig.ObjectMeta.DeletionTimestamp)
+	log.Infof("Fargate instance group state: %s", ig.GetState())
+	log.Infof("Fargate profile state: %s", profileStatus)
 
-	if ig.GetState() == v1alpha1.ReconcileReady {
-		var err error = nil
-		// Delete request
-		if !ig.ObjectMeta.DeletionTimestamp.IsZero() {
-			err = worker.Delete()
-			if err != nil {
-				ig.SetState(v1alpha1.ReconcileErr)
-			} else {
-				ig.SetState(v1alpha1.ReconcileDeleting)
-			}
-		}
-		return err
+	if transitioner, ok := sm[hash(string(ig.GetState()), profileStatus)]; ok {
+		return transitioner(ig, worker)
 	}
-
 	return nil
+
 }
