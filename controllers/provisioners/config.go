c12516f5ea2017c84ecbcdbef3bbaa0f30b1cd8d
Eytan Avisror <eytan_avisror@intuit.com>
2020-07-01 11:28:23 -0700

    less logging on configmap reconciler

--- controllers/provisioners/config.go
+++ controllers/provisioners/config.go
@@ -16,6 +16,7 @@ limitations under the License.
 package provisioners
 
 import (
+	"reflect"
 	"strings"
 
 	"github.com/ghodss/yaml"
@@ -42,12 +43,16 @@ func SetConfigurationDefaults(instanceGroup *v1alpha1.InstanceGroup, config *Def
 		modifiedInstanceGroup = &v1alpha1.InstanceGroup{}
 	)
 
+	if reflect.DeepEqual(config, &DefaultConfiguration{}) {
+		return instanceGroup, nil
+	}
+
+	log.Info("applying defaults from configmap")
+
 	unstructuredInstanceGroup, err := runtime.DefaultUnstructuredConverter.ToUnstructured(instanceGroup)
 	if err != nil {
 		return instanceGroup, errors.Wrap(err, "failed to convert instance group to unstructured")
 	}
-
-	log.Info("applying restricted defaults", "paths", config.Boundaries.Restricted)
 
 	// apply restricted paths to instance group
 	for _, pathStr := range config.Boundaries.Restricted {
@@ -55,15 +60,12 @@ func SetConfigurationDefaults(instanceGroup *v1alpha1.InstanceGroup, config *Def
 		// if a default value exists for the path, set it on the instance group
 		if field, ok, _ := unstructured.NestedFieldCopy(config.Defaults, path...); ok {
 			// default value exists for restricted path
-			log.Info("setting field", "field", field, "path", path)
 			err := unstructured.SetNestedField(unstructuredInstanceGroup, field, path...)
 			if err != nil {
 				return instanceGroup, errors.Wrap(err, "failed to set field")
 			}
 		}
 	}
-
-	log.Info("applying shared defaults", "paths", config.Boundaries.Shared)
 
 	// merge shared paths with instance group
 	for _, pathStr := range config.Boundaries.Shared {
@@ -85,14 +87,12 @@ func SetConfigurationDefaults(instanceGroup *v1alpha1.InstanceGroup, config *Def
 		}
 
 		if crField == nil {
-			log.Info("setting shared field", "field", defaultField, "path", path)
 			if err := unstructured.SetNestedField(unstructuredInstanceGroup, defaultField, path...); err != nil {
 				return instanceGroup, errors.Wrap(err, "failed to set field")
 			}
 			continue
 		}
 
-		log.Info("merging shared fields", "field", defaultField, "resource", crField, "path", path)
 		switch v := defaultField.(type) {
 		case []interface{}:
 			ifaceSlc := crField.([]interface{})
