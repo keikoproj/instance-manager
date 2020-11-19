d84908b7bb5a0b4d341b52e27b4f5aff43e21c0e
Eytan Avisror <eytan.avisror@hotmail.com>
2020-11-18 18:48:16 -0800

    get launchID from scaling group (#209)
    Signed-off-by: Eytan Avisror <Eytan_Avisror@hotmail.com>

--- controllers/providers/aws/aws.go
+++ controllers/providers/aws/aws.go
@@ -1254,3 +1254,15 @@ func GetInstanceFamily(instanceType string) string {
 	}
 	return ""
 }
+
+func GetScalingConfigName(group *autoscaling.Group) string {
+	var configName string
+	if IsUsingLaunchConfiguration(group) {
+		configName = aws.StringValue(group.LaunchConfigurationName)
+	} else if IsUsingLaunchTemplate(group) {
+		configName = aws.StringValue(group.LaunchTemplate.LaunchTemplateName)
+	} else if IsUsingMixedInstances(group) {
+		configName = aws.StringValue(group.MixedInstancesPolicy.LaunchTemplate.LaunchTemplateSpecification.LaunchTemplateName)
+	}
+	return configName
+}
