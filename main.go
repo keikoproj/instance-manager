11fcdff899e39155117b44b91f97dd7f35baf9fe
Jonah Back <jonah@jonahback.com>
2022-04-06 11:14:22 -0400

    chore: release 0.14.1
    Signed-off-by: Jonah Back <jonah@jonahback.com>

--- main.go
+++ main.go
@@ -44,7 +44,7 @@ var (
 	setupLog = ctrl.Log.WithName("setup")
 )
 
-const controllerVersion = "instancemgr-0.14.0"
+const controllerVersion = "instancemgr-0.14.1"
 
 func init() {
 	instancemgrv1alpha1.AddToScheme(scheme)
